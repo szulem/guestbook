d3540f3808c75ee1749c8003c3fa85c84ad523d5
Mirek <mirek@szul.eu>
2018-09-11 11:48:58 +0200

    Post controller spec refactoring

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -3,52 +3,59 @@ require "rails_helper"
 describe PostsController do
 
   describe '#create' do
+    
+    subject { post.save }
+    let(:author) { "Mirek" }
+    let(:body) { "0123456789" }
+    let(:post) { Post.new(author: author, body: body) }
+    
     context 'post is valid' do
-      let(:post) { Post.new(author: "Mirek", body: "0123456789") }
       it 'returns true' do
-        expect(post.save).to eql(true)
+        is_expected.to eql(true)
       end
     end
 
     context 'author is empty' do
-      let(:post) { Post.new(author: "", body: "0123456789") }
+      let(:author) { "" }
       it 'returns false' do
-        expect(post.save).to eql(false)
+        expect(subject).to eql(false)
       end
     end
 
     context 'body is empty' do
-      let(:post) { Post.new(author: "Mirek", body: "") }
+      let(:body) { "" }
       it 'returns false' do
-        expect(post.save).to eql(false)
+        expect(subject).to eql(false)
       end
     end
 
     context 'author and body are empty' do
-      let(:post) { Post.new(author: "", body: "") }
+      let(:author) { "" }
+      let(:body) { "" }
       it 'returns false' do
-        expect(post.save).to eql(false)
+        expect(subject).to eql(false)
       end
     end
 
     context 'author is too short' do
-      let(:post) { Post.new(author: "a", body: "0123456789") }
+      let(:author) { "a" }
       it 'returns false' do
-        expect(post.save).to eql(false)
+        expect(subject).to eql(false)
       end
     end
 
     context 'body is too short' do
-      let(:post) { Post.new(author: "Mirek", body: "0123") }
+      let(:body) { "0123" }
       it 'returns false' do
-        expect(post.save).to eql(false)
+        expect(subject).to eql(false)
       end
     end
 
     context 'author and body are too short' do
-      let(:post) { Post.new(author: "a", body: "0123") }
+      let(:author) { "a" }
+      let(:body) { "0123" }
       it 'returns false' do
-        expect(post.save).to eql(false)
+        expect(subject).to eql(false)
       end
     end
   end
